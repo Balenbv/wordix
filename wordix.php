06e0ab7cda91d972e73396f03546544b30d28fab
riosmerinoml <riosmerinoml@gmail.com>
2023-11-13 04:04:22 -0300

    Agregué una línea a la función 5

--- wordix.php
+++ wordix.php
@@ -36,7 +36,7 @@ const ESTADO_LETRA_PERTENECE = "pertenece";
 function solicitarNumeroEntre($min, $max)
 {
     //int $numero
-
+    echo "\nIngrese un número entre " . $min . " y " . $max . ": ";
     $numero = trim(fgets(STDIN));
 
     if (is_numeric($numero)) { //determina si un string es un número. puede ser float como entero.
