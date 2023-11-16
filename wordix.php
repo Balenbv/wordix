60e2fb3d4e2381a2c2ae005471ba2323324f01ab
riosmerinoml <riosmerinoml@gmail.com>
2023-11-16 20:40:31 -0300

    Puntajes corregidos

--- wordix.php
+++ wordix.php
@@ -356,7 +356,7 @@ function obtenerPuntajeWordix($cantIntentos, $palabraEncontrada)
 
     $puntosLetras = ["A"=>1,"E"=>1,"I"=>1,"O"=>1,"U"=> 1,
     "B"=>2,"C"=>2,"D"=>2,"F"=>2,"G"=>2,"H"=>2,"J"=>2,"K"=>2,"L"=>2,"M"=>2,
-    "N"=>1,"P"=>1,"Q"=>1,"R"=>1,"S"=>1,"T"=>1,"V"=>1,"W"=>1,"X"=>1,"Y"=>1,"Z"=>1 ];
+    "N"=>3,"P"=>3,"Q"=>3,"R"=>3,"S"=>3,"T"=>3,"V"=>3,"W"=>3,"X"=>3,"Y"=>3,"Z"=>3 ];
 
     for ($i=0; $i < 5; $i++){
         $letraDePalabraEncontrada = $palabraEncontrada[$i];
